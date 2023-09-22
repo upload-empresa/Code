e93bdab77ddccb259575fde1b04c034b7d0eccbb
HelenaLannes18 <helena.lannes.salles@hotmail.com>
2023-09-22 09:01:40 -0300

    login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import { NextPageContext } from 'next';
 import { getSession, signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
 
-
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
 
@@ -29,11 +28,6 @@ const Auth = () => {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
 
-  const [variant, setVariant] = useState('login');
-
-  const toggleVariant = useCallback(() => {
-    setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
-  }, []);
 
   const login = useCallback(async () => {
     try {
@@ -50,19 +44,6 @@ const Auth = () => {
     }
   }, [email, password, router]);
 
-  const register = useCallback(async () => {
-    try {
-      await axios.post('/api/register', {
-        email,
-        name,
-        password
-      });
-
-      login();
-    } catch (error) {
-      console.log(error);
-    }
-  }, [email, name, password, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -73,17 +54,9 @@ const Auth = () => {
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
-              {variant === 'login' ? 'Sign in' : 'Register'}
+              Login
             </h2>
             <div className="flex flex-col gap-4">
-              {variant === 'register' && (
-                <input
-                  id="name"
-                  type="text"
-                  value={name}
-                  onChange={(e: any) => setName(e.target.value)}
-                />
-              )}
               <input
                 id="email"
                 type="email"
@@ -97,17 +70,10 @@ const Auth = () => {
                 onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
-            <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-              {variant === 'login' ? 'Login' : 'Sign up'}
+            <button onClick={login} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              Login
             </button>
 
-            <p className="text-neutral-500 mt-12">
-              {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
-              <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
-                {variant === 'login' ? 'Create an account' : 'Login'}
-              </span>
-              .
-            </p>
           </div>
         </div>
       </div>
---
+++ src/pages/login.tsx
@@ -0,0 +1,84 @@
+import axios from 'axios';
+import { useCallback, useState } from 'react';
+import { NextPageContext } from 'next';
+import { getSession, signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
+
+export async function getServerSideProps(context: NextPageContext) {
+    const session = await getSession(context);
+
+    if (session) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
+
+    return {
+        props: {}
+    }
+}
+
+const Auth = () => {
+    const router = useRouter();
+
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+
+            router.push('/profiles');
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, password, router]);
+
+
+    return (
+        <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+            <div className="bg-black w-full h-full lg:bg-opacity-50">
+                <nav className="px-12 py-5">
+                    <img src="/images/logo.png" className="h-12" alt="Logo" />
+                </nav>
+                <div className="flex justify-center">
+                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                        <h2 className="text-white text-4xl mb-8 font-semibold">
+                            Login
+                        </h2>
+                        <div className="flex flex-col gap-4">
+                            <input
+                                id="email"
+                                type="email"
+                                value={email}
+                                onChange={(e: any) => setEmail(e.target.value)}
+                            />
+                            <input
+                                type="password"
+                                id="password"
+                                value={password}
+                                onChange={(e: any) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <button onClick={login} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                            Login
+                        </button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Auth;
